58f0a8b99c8fa9d9a0c2c2e09459eb7e8622207a
Jeremiahtaylor2017 <37142633+Jeremiahtaylor2017@users.noreply.github.com>
2025-11-12 16:01:51 -0800

    removing ssh key auth

--- pkg/jobs/types.go
+++ pkg/jobs/types.go
@@ -3,6 +3,7 @@ package jobs
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -24,9 +25,15 @@ type JobDefinition struct {
 	Command    string   `yaml:"command" json:"command"`
 	Arguments  []string `yaml:"arguments" json:"arguments"`
 	// Controls whether the engine requests a pseudo-terminal for interactive commands
-	AllowTTY bool              `yamlL:"allow_tty" json:"allow_tty"`
-	Checksum string            `yaml:"checksum" json:"checksum"`
-	Metadata map[string]string `yaml:"metadata" json:"metadata"`
+	AllowTTY    bool              `yamlL:"allow_tty" json:"allow_tty"`
+	Checksum    string            `yaml:"checksum" json:"checksum"`
+	Metadata    map[string]string `yaml:"metadata" json:"metadata"`
+	Credentials CredentialBundle  `yaml:"credentials" json:"credentials"`
+}
+
+type CredentialBundle struct {
+	Username string `yaml:"username" json:"username"`
+	Password string `yaml:"password" json:"password"`
 }
 
 type Result struct {
@@ -57,5 +64,19 @@ func (j JobDefinition) Validate() error {
 	if j.Checksum == "" {
 		return fmt.Errorf("job %s missing checksum", j.ID)
 	}
+	if err := j.Credentials.Validate(); err != nil {
+		return fmt.Errorf("job %s credentials invalid: %w", j.ID, err)
+	}
+	return nil
+}
+
+func (c CredentialBundle) Validate() error {
+	if strings.TrimSpace(c.Username) == "" {
+		return errors.New("username required")
+	}
+	if strings.TrimSpace(c.Password) == "" {
+		return errors.New("password required")
+	}
+
 	return nil
 }
